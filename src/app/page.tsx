04f2794bfe42a5616b0d2a5e26decbf57556983c
chanwoong1 <jcw7468@gmail.com>
2023-12-21 13:34:49 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 import Image from 'next/image';
 import styles from './page.module.css'
-import image1 from '@static/web_image1.png'
-import image2 from '@static/web_image2.png'
-import image3 from '@static/web_image3.png'
 import { useEffect } from 'react';
 
 export default function Home() {
@@ -32,11 +29,11 @@ export default function Home() {
   return (
     <div>
       <h1 className={styles.description}>세상 모든 장소에 왔다감</h1>
-      <Image src={image1} alt="image1" className={styles.image} />
+      <Image src="https://github.com/WatDaGam/WatDaGam.github.io/blob/main/src/assets/images/web_image1.png?raw=true" alt="image1" className={styles.image} />
       <h1 className={styles.description}>당신의 마음에도 왔다감</h1>
-      <Image src={image2} alt="image2" className={styles.image} />
+      <Image src="https://github.com/WatDaGam/WatDaGam.github.io/blob/main/src/assets/images/web_image2.png?raw=true" alt="image2" className={styles.image} />
       <h1 className={styles.description}>당신이 머물렀던 곳을 공유해보세요</h1>
-      <Image src={image3} alt="image3" className={styles.image} />
+      <Image src="https://github.com/WatDaGam/WatDaGam.github.io/blob/main/src/assets/images/web_image3.png?raw=true" alt="image3" className={styles.image} />
     </div>
   )
 }

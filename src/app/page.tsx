ccbe8d7a1a531656b50630063b7bf18fe174eeea
chanwoong1 <jcw7468@gmail.com>
2023-12-21 12:03:13 +0900

    배포 환경 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,9 @@ export default function Home() {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          entry.target.style.opacity = 1;
-          entry.target.style.transform = 'translateY(0px)';
+          const target = entry.target as HTMLElement;
+          target.style.opacity = '1';
+          target.style.transform = 'translateY(0px)';
         }
       });
     }, {
